22742b234133ad7de83be2311da38ecf74b1af09
Bradley Snyder <snyder.bradleyj@gmail.com>
2018-08-27 02:06:53 -0700

    Add license

--- led_timer.c
+++ led_timer.c
@@ -1,15 +1,37 @@
+/**
+ * Copyright 2018 Bradley J. Snyder <snyder.bradleyj@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 
+// Note, ATmega328p was used for development purposes and is not guaranteed to work.
+// Use ATtiny85 for proper operation.
 // ATtiny85 fuses -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
 // ATmega328p fuses -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xfd:m
 
 #define SLEEP_TIMER_DEFAULT_DURATION (60L*60*6)
 #define WAKEUP_TIMER_DURATION (60L*60*24)
 
-//#define BUTTON_DEBOUNCE_SAMPLES (F_CPU / 400000) // 50 @ 20MHz
 #define BUTTON_DEBOUNCE_SAMPLES 1
 #define FAST_FADE_TIME_MS 2000
 #define OFF_MIN_BRIGHT 0
